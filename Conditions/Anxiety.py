c09541cb51550288d12dfdd0e8da8419337358d4
Mahshid zamanvaziri <zamanvaziri.mailbox@gmail.com>
2025-07-01 17:55:20 +0200

    Create Anxiety.py

---
+++ Conditions/Anxiety.py
@@ -0,0 +1,13 @@
+from Utils.score_gad7 import score_gad7
+import json
+
+with open("Tests/gad7.json", "r", encoding="utf-8") as f:
+    gad7 = json.load(f)
+
+def start_screening(chat_history):
+    return "حالت چطوره؟ می‌خوای درباره احساست صحبت کنی؟", "symptom_check"
+
+def handle_input(user_input, chat_history):
+    if any(word in user_input for word in ["تست", "بله", "اوکی"]):
+        return "بسیار خوب، تست اضطراب GAD-7 را شروع می‌کنیم.", "test_active"
+    return "به نظر می‌رسه اضطرابی وجود داره. تمایل داری یک تست کوتاه بدیم؟", "test_offer"

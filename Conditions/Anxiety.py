85f43f802ed1635bb7a60cb76271911db139a136
Mahshid zamanvaziri <zamanvaziri.mailbox@gmail.com>
2025-07-02 00:07:44 +0200

    Update Anxiety.py

--- Conditions/Anxiety.py
+++ Conditions/Anxiety.py
@@ -8,6 +8,6 @@ def start_screening(chat_history):
     return "حالت چطوره؟ می‌خوای درباره احساست صحبت کنی؟", "symptom_check"
 
 def handle_input(user_input, chat_history):
-    if any(word in user_input for word in ["تست", "بله", "اوکی"]):
-        return "بسیار خوب، تست اضطراب GAD-7 را شروع می‌کنیم.", "test_active"
+    if any(word in user_input for word in ["تست", "بله", "حتما", "اره","آره", "باشه","اوکی"]):
+        return "بسیار خوب، تست اضطراب را شروع می‌کنیم.", "test_active"
     return "به نظر می‌رسه اضطرابی وجود داره. تمایل داری یک تست کوتاه بدیم؟", "test_offer"

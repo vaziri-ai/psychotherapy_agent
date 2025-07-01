12bbb5a0cde227ca171e1df88d870593bd8c4e61
Mahshid zamanvaziri <zamanvaziri.mailbox@gmail.com>
2025-07-01 23:46:11 +0200

    Update process_input.py

--- process_input.py
+++ process_input.py
@@ -6,12 +6,12 @@ def process_user_input(user_input, step, chat_history):
         return anxiety.start_screening(chat_history)
     
     if "اضطراب" in user_input or any(word in user_input for word in ["بی‌قراری", "تپش قلب", "دل‌درد"]):
-        return anxiety.handle_input(user_input, chat_history)
+        return Anxiety.handle_input(user_input, chat_history)
 
     elif "تمرکز" in user_input or "حواس‌پرتی" in user_input:
-        return adhd.handle_input(user_input, chat_history)
+        return ADHD.handle_input(user_input, chat_history)
 
     elif "وسواس" in user_input or "شستن دست" in user_input:
-        return ocd.handle_input(user_input, chat_history)
+        return OCD.handle_input(user_input, chat_history)
 
     return None, step
